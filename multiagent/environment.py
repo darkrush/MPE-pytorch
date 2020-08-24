98605ab516c0f59a19242b02a516d98c5ef6e789
darkrush <kidfatenight@gmail.com>
2020-08-24 16:36:11 +0800

    ns

--- multiagent/environment.py
+++ multiagent/environment.py
@@ -85,12 +85,12 @@ class MultiAgentEnv(gym.Env):
                 self.action_space.append(total_action_space[0])
             # observation space
             obs_temp = observation_callback(agent, self.world)
-            if isinstance(obs_temp,tuple):
-                obs_dim = len(obs_temp[0])
-                global_dim = len(obs_temp[1])
+            if isinstance(obs_temp,dict):
+                obs_dim = len(obs_temp['p_obs'])
+                global_dim = len(obs_temp['g_obs'])
                 p_space = spaces.Box(low=-np.inf, high=+np.inf, shape=(obs_dim,), dtype=np.float32)
                 g_space = spaces.Box(low=-np.inf, high=+np.inf, shape=(global_dim,), dtype=np.float32)
-                self.observation_space.append([p_space,g_space])
+                self.observation_space.append({'p_obs': p_space, 'g_obs': g_space})
             else:
                 obs_dim = len(obs_temp)
                 self.observation_space.append(spaces.Box(low=-np.inf, high=+np.inf, shape=(obs_dim,), dtype=np.float32))#[-inf,inf]
